0473a848ad90ff22863a3d2dab4288855ee3711c
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-11-05 10:44:56 +0400

    modified code

--- SQLIntro.sql
+++ SQLIntro.sql
@@ -8,5 +8,6 @@ Surname NVARCHAR(25),
 Age INT,
 AvgPoint INT
 ) 
+INSERT INTO Students VALUES ('Nuray', 'Quliyeva', 25, 45)
 SELECT * FROM Students WHERE Name like 'A%f'
 SELECT * FROM Students WHERE AvgPoint > 51
